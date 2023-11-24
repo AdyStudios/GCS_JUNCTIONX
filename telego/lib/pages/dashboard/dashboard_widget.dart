56e5b57bb11aa9b615f02d65cfcbef501e0e6bfe
AdyStudios <karaadamka@gmail.com>
2023-11-25 00:35:17 +0100

    add logo

--- telego/lib/pages/dashboard/dashboard_widget.dart
+++ telego/lib/pages/dashboard/dashboard_widget.dart
@@ -9,12 +9,8 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-import '../../user.dart';
-
 import 'dashboard_model.dart';
 export 'dashboard_model.dart';
-
-var l_user = User(69420, "Kara", "+36305789426");
 
 class DashboardWidget extends StatefulWidget {
   const DashboardWidget({Key? key}) : super(key: key);
@@ -74,24 +70,56 @@ class _DashboardWidgetState extends State<DashboardWidget>
           : FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        appBar: AppBar(
-          backgroundColor: FlutterFlowTheme.of(context).primary,
-          automaticallyImplyLeading: false,
-          title: Align(
-            alignment: AlignmentDirectional(0.00, 0.00),
-            child: Text(
-              'Dashboard',
-              style: FlutterFlowTheme.of(context).headlineMedium.override(
-                    fontFamily: 'Outfit',
-                    color: Colors.white,
-                    fontSize: 22,
+        backgroundColor: Color(0xFF404040),
+        appBar: PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.sizeOf(context).height * 0.1),
+          child: AppBar(
+            backgroundColor: Color(0xFFF7B644),
+            automaticallyImplyLeading: false,
+            title: Align(
+              alignment: AlignmentDirectional(0.00, 0.00),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(0.00, 0.00),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        '../../../assets/img/Layer_1.png',
+                        width: 65,
+                        height: 65,
+                        fit: BoxFit.cover,
+                        alignment: Alignment(0.00, 0.00),
+                      ),
+                    ),
                   ),
+                  Align(
+                    alignment: AlignmentDirectional(0.00, 0.00),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                      child: Text(
+                        'Dashboard',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.of(context)
+                            .headlineMedium
+                            .override(
+                              fontFamily: 'Outfit',
+                              color: Color(0xFFE8E9EB),
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
+            actions: [],
+            centerTitle: false,
+            toolbarHeight: MediaQuery.sizeOf(context).height * 0.1,
+            elevation: 2,
           ),
-          actions: [],
-          centerTitle: false,
-          elevation: 2,
         ),
         body: SafeArea(
           top: true,
@@ -107,14 +135,17 @@ class _DashboardWidgetState extends State<DashboardWidget>
                   maxHeight: MediaQuery.sizeOf(context).height * 0.1,
                 ),
                 decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).secondaryBackground,
+                  color: Color(0xFF404040),
                 ),
                 child: Align(
                   alignment: AlignmentDirectional(0.00, 0.00),
                   child: Text(
-                    l_user.name,
+                    'Kara Adam',
                     textAlign: TextAlign.center,
-                    style: FlutterFlowTheme.of(context).bodyMedium,
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Readex Pro',
+                          color: Color(0xFFE8E9EB),
+                        ),
                   ),
                 ),
               ),
@@ -128,48 +159,35 @@ class _DashboardWidgetState extends State<DashboardWidget>
                   maxHeight: MediaQuery.sizeOf(context).height * 0.2,
                 ),
                 decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).secondaryBackground,
+                  color: Color(0xFF404040),
                 ),
                 child: Align(
                   alignment: AlignmentDirectional(0.00, 0.00),
                   child: Text(
-                     l_user.points.toString() + ' POINTS',
+                    "69400 POINTS",
                     textAlign: TextAlign.center,
                     style: FlutterFlowTheme.of(context).bodyMedium.override(
                           fontFamily: 'Readex Pro',
+                          color: Color(0xFFF7B644),
                           fontSize: 60,
                         ),
                   ).animateOnPageLoad(
                       animationsMap['textOnPageLoadAnimation']!),
                 ),
               ),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).secondaryBackground,
-                ),
-                child: Align(
-                  alignment: AlignmentDirectional(0.00, 0.00),
-                  child: Text(
-                    l_user.phone,
-                    style: FlutterFlowTheme.of(context).bodyMedium,
-                  ),
-                ),
-              ),
               FFButtonWidget(
                 onPressed: () {
-                  Navigator.pushNamed(context, '/daily_challanges');
+                  print('Button pressed ...');
                 },
-                text: 'Daily Challanges',
+                text: 'Get More Points',
                 options: FFButtonOptions(
                   height: 40,
                   padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
                   iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-                  color: FlutterFlowTheme.of(context).primary,
-                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                  color: Color(0xFF498C8A),
+                  textStyle: FlutterFlowTheme.of(context).titleMedium.override(
                         fontFamily: 'Readex Pro',
-                        color: Colors.white,
+                        color: Color(0xFFE8E9EB),
                       ),
                   elevation: 3,
                   borderSide: BorderSide(
@@ -177,6 +195,23 @@ class _DashboardWidgetState extends State<DashboardWidget>
                     width: 1,
                   ),
                   borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color(0xFF404040),
+                ),
+                child: Align(
+                  alignment: AlignmentDirectional(0.00, 0.00),
+                  child: Text(
+                    'PHONE NUMBER',
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Readex Pro',
+                          color: Color(0xFFE8E9EB),
+                        ),
+                  ),
                 ),
               ),
             ],
