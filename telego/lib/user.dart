dc0c1bd13c0f8726c5682c1ce69c4617a7567bde
AdyStudios <karaadamka@gmail.com>
2023-11-24 23:41:29 +0100

    this sucks

--- telego/lib/user.dart
+++ telego/lib/user.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class User{
   int points=0;
-  String name="Name";
-  String phone="+36305789426";
+  String name="";
+  String phone="";
   User(this.points,this.name,this.phone);
   int getPoints(){return points;}
   String getName(){return name;}
